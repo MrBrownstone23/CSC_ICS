1fe8ec3742159f1ffed7a14128bfbdd01bd5abcc
Cory <Cory@DESKTOP-IU3M4L3>
2016-07-22 22:22:49 -0500

    PArts class

---
+++ WindowsFormsApplication1/SearchPartsGrid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static WindowsFormsApplication1.Enums;
+
+namespace WindowsFormsApplication1
+{
+    public class SearchPartsGrid
+    {
+
+
+        public List<int> QuickID { get; set; }
+        public List<string> PartNumber { get; set; }
+        public List<string> SerialNumber { get; set; }
+        public List<string> PartDescription { get; set; }
+        public List<string> Location { get; set; }
+        public List<string> Category { get; set; }
+        public List<string> Quantity { get; set; }
+        
+
+    }
+}
